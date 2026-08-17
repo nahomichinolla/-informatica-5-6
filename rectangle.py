53ca36271080c1d65a5726fe79f414add89662aa
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-08-17 14:59:11 +0000

    rectangle

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+def main() :
+    w = int(input("Width: "))
+    print("O" * w)
+    print("O" * w)
+    print("O" * w)
+    print("O" * w)
+    print("O" * w)
+
+if __name__ == "__main__":
+    main()
