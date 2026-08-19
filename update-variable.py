066322184bffecb8ee4c98e260c8f84e5795c72e
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-08-19 18:01:04 +0000

    operators

---
+++ update-variable.py
@@ -0,0 +1,34 @@
+def main():
+    # Augmented Assignment Operator
+    # x+= y
+    score = 10
+    score += 10
+    print(score)
+
+    # Subtracton Assignent Operator
+    # x -=y
+    score = 100
+    score -= 15
+    print(score)
+
+    # Multipication Assignment Operator
+    # x *= y
+    balance = 10
+    balance *= 4
+    print(balance)
+
+    #Division Assignment Operator
+    # x /= y
+    number = 10
+    number /= 5
+    print(number)
+
+    # Modulus Assignment Operator
+    a = 10
+    b = 4
+
+    a %= b
+    print(a)
+
+if __name__ == "__main__":
+    main()
