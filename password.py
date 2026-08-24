e115e4fe8700205fc6c29f9e3c327dc3e5d3270d
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-08-24 15:00:26 +0000

    password

---
+++ password.py
@@ -0,0 +1,13 @@
+def main() :
+    password = 7777
+    user = int(input("What is the password? "))
+    if user == password:
+        print("Correct password")
+
+
+    print("See you later!")
+
+
+
+if __name__ == "__main__":
+    main()
