e2b63c303d3fd94b674be866cdc31f0ee90827c6
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-08-26 18:10:21 +0000

    medium

---
+++ if-exercises-medium.py
@@ -0,0 +1,14 @@
+def main() :
+    number1 = float(input("Enter a number: "))
+    number2 = float(input("Enter another number: "))
+    operation = input("Enter an operation: ")
+
+    if operation = "addition":
+        print(number1 + number2)
+    elif operation = "subtraction":
+        print(number1 - number2)
+    elif operation = "multiply
+
+
+if __name__ == "__main__":
+    main()
