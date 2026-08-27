5d5b616a1ad59e34628f20565ec67b3454c01bc8
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-08-27 15:43:35 +0000

    test

--- atmosphere.py
+++ atmosphere.py
@@ -1,21 +1,39 @@
 def main() :
+    print("Descent atmosphere layer: ")
     layer = input("Descent atmosphere layer: ")
-    if layer == "Exosphere"
-        print("Your altiltitude level will be between 700 and 10000")
-    elif layer == "Mesosphere"
-        print("Your altitude level will be between 50 and 700km ")
-    elif layer == "Thermosphere"
-        print("Your altitude will be between 85 and 700km)
-    elif layer == "Stratosphere"
-        print("Your altitude will be between 12 and 500")
-    elif layer == "Troposhere"
-        print("Your altitude will be between 0 and 12km")
-    else
-        print("Insert an atmosphere layer: ")
-    exact = input("Enter exact altitude")
+    if layer == Exosphere:
+        print("Your altiltitude level will be between 700 - 10000km")
+    elif layer == "Mesosphere":
+        print("Your altitude level will be between 50 - 85km")
+    elif layer == "Thermosphere":
+        print("Your altitude will be between 85 - 700km")
+    elif layer == "Stratosphere":
+        print("Your altitude will be between 12 - 50km")
+    elif layer == "Troposhere":
+        print("Your altitude will be between 0 - 12km")
+    else:
+        print("Insert a valid atmosphere layer: ")
+    exact = input("Enter exact altitude: ")
+    ex = 2000
+    me = 200
+    th = 500
+    st = 75
+    tr = 20
+    if exact > 699:
+        a1 = (exact*1000)/ex
+        a2 = 35000/me
+        a3 = 30000/st
+        a4 = 12000/tr
+        a5 = 61500/th
+        a6 = a2 + a3 + a4 + a5
+        print(a2 + a3 + a4 + a5, "seconds")
+    elif exact > 84:
+
 
 
     print("Enter exact altitude: ")
+    if
+        print("Total descend time:")
 
 if __name__ == "__main__":
     main()
