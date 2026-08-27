bfb16ee6f35638ef77b3a14a164046fbebd13b03
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-08-27 15:00:14 +0000

    test

---
+++ atmosphere.py
@@ -0,0 +1,21 @@
+def main() :
+    layer = input("Descent atmosphere layer: ")
+    if layer == "Exosphere"
+        print("Your altiltitude level will be between 700 and 10000")
+    elif layer == "Mesosphere"
+        print("Your altitude level will be between 50 and 700km ")
+    elif layer == "Thermosphere"
+        print("Your altitude will be between 85 and 700km)
+    elif layer == "Stratosphere"
+        print("Your altitude will be between 12 and 500")
+    elif layer == "Troposhere"
+        print("Your altitude will be between 0 and 12km")
+    else
+        print("Insert an atmosphere layer: ")
+    exact = input("Enter exact altitude")
+
+
+    print("Enter exact altitude: ")
+
+if __name__ == "__main__":
+    main()
