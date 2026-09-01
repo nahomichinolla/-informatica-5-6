9c87291687e09379fb3e3fe8135f157a5d83a8e5
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-01 21:41:52 +0000

    game2

---
+++ coinfip2.py
@@ -0,0 +1,21 @@
+import random
+def main():
+    coin = ["heads","tails"]
+    attempts = 3
+    while attempts > 0:
+        flip = random.choice(coin)
+        guess = input("Heads or tails?: ").strip().lower()
+
+        print("The coin landed on", flip)
+
+        if guess == flip:
+            print("Winner")
+            break
+        else:
+            print("loser")
+            attempts -= 1
+            print("Attempts left:", attempts)
+
+if __name__ == "__main__":
+    main()
+
