d6141896bda0e1a87217a9a8406e1e746add9e40
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-02 18:04:54 +0000

    almost

--- roadtrip.py
+++ roadtrip.py
@@ -1,9 +1,14 @@
 def main():
+    # Finite Loop
     answer = ""
-
+    followup = ""
     while answer != "Yes!":
         answer = input("Are we there yet? ").title().strip()
-        
+        if answer == "Yes":
+            followup = input("Really? ").title().strip()
+        if followup == "Yes":
+            break
+
     print("We are here!")
 
 if __name__ == "__main__":
