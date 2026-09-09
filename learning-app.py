888a4914950dc94859c9927d194595fc4f15c4fd
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-09 18:08:53 +0000

    PP

--- learning-app.py
+++ learning-app.py
@@ -2,25 +2,26 @@ import random
 def main():
     #App Name
     print("Addition App⭐")
-    print("Welcome!")
-    num1 = random.randint(10, 99)
-    num2 = random.randint(10, 99)
+    print("Welcome! Here you will learn math")
+    print("Select your operation type")
+    problem = input("(+)")
+    streak = 0
 
-    correctanswer = num1 + num2
-
-    print(f"What is {num1} + {num2}?")
-    answer = int(input("Your answer: "))
-    if answer == correctanswer:
-        print("Correct!")
-        streak += 1
-        print("Streak:", "⭐"*streak)
-    
-
-
-
-
-
-
+    if problem == "+":
+        while streak < 3:
+            num1 = random.randint(10,90)
+            num2 = random.randint(10,90)
+            print("Solve: ")
+            correct = num1 + num2
+            answer = int(input(f"{num1} + {num2}: "))
+            if answer != correct:
+                streak = 0
+                print(f"correct asnwer:{correct}")
+                print(f"Streak:{streak}")
+            elif answer == correct:
+                print("Good job!")
+                streak += 1
+                print(f"streak:{streak}")
 
 if __name__ == "__main__":
     main()
