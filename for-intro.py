1624e7e775f92eb3abcec7a2d0a25bedb5d956d9
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-18 20:16:28 +0000

    f

---
+++ for-intro.py
@@ -0,0 +1,14 @@
+def main():
+    doctrine = ["Faith in the Lord Jesus Christ", "Repentance", "Baptism", "Impocicion de manos", "Perseverar hasta el fin"]
+
+    index = 0
+
+    # while index < len(doctrine): Condition
+    #   print(doctrine[index])
+    #   index += 1 Update
+
+    for principles in doctrine:
+        print(principles)
+
+if __name__ == "__main__":
+    main()
