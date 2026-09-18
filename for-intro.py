5be26cab8acac37eba4f440fc2ba6924585bf852
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-18 21:22:04 +0000

    f

--- for-intro.py
+++ for-intro.py
@@ -7,8 +7,8 @@ def main():
     #   print(doctrine[index])
     #   index += 1 Update
 
-    for principles in doctrine:
-        print(principles)
+    for i in range(len(doctrine)):
+        print(f"{i+1} {doctrine[i]}")
 
 if __name__ == "__main__":
     main()
