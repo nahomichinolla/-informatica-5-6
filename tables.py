f6b5a7b1cc5c26ba898248ba37df246c0829906d
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-24 14:38:09 +0000

    k

--- tables.py
+++ tables.py
@@ -3,10 +3,11 @@ def main():
         num = input("Enter a number (1-10): ")
 
         num = int(num)
-        print("Here is the", num, "times table")
+        print("Here is the {num} times table")
 
-        for i in range(1, 11):
-              print(i, "times", num, "is", i * num)
+        for x in range(11):
+              answer = x * num
+              print(f"{x} times {num} is {answer}")
 
 
 
