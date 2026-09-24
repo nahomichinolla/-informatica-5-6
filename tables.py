2fe12a594e72ab8f88ece4285ca6f1586406a13c
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-09-24 15:22:10 +0000

    g

--- tables.py
+++ tables.py
@@ -1,17 +1,23 @@
 def main():
-        while True:
+        valid_nums = []
+        for i in range(1,11):
+             valid_nums.append(str(i))
 
-            times_table = input("Enter a number: ").lower().strip()
+        while True:
+            times_table = input("Enter a number (1-10 or exit): ").lower().strip()
 
             if times_table == "exit":
                  break
-            else:
+            elif times_table in valid_nums:
                 max_value = int(input("Enter a maximum value for the times table: "))
 
                 print(f"Here is the {times_table} times table")
 
                 for x in range(1, max_value + 1):
                     answer = x * int(times_table)
+                    print(f"{x} times {times_table} is {answer}")
+            else:
+                 print("Invalid command.")
 
 
 if __name__=="__main__":
